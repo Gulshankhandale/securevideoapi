2349dd4fc158a7ba5bdcbdc9b4e798b37709d12c
Gulshan <gulshankhandale.btech@gmail.com>
2023-10-13 19:32:09 +0530

    changes

---
+++ seeders/20231013135354-video-data.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await queryInterface.bulkInsert('videos', [
+    {
+      name: 'video1',
+      url: '/storage/videos/sample-5s.mp4',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'video2',
+      url: '/storage/videos/sample-5s.mp4',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'video3',
+      url: '/storage/videos/sample-5s.mp4',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+   ])
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
